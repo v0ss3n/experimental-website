51235da300bc70e40754971f3586428d7d8580ec
miv <m.vossen@live.nl>
2019-01-15 17:56:03 +0100

    Update of landing page and portfolio page, add slick

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-
-$('.tabs-wrapper').each((a, tabsWrapperEl) => {
+$(".tabs-wrapper").each((a, tabsWrapperEl) => {
   const $tabsWrapperEl = $(tabsWrapperEl);
-  $tabsWrapperEl.find('.tab__title').each((i, titleWrapperEl) => {
+  $tabsWrapperEl.find(".tab__title").each((i, titleWrapperEl) => {
     const $title = $(titleWrapperEl);
-    $title.on('click', () => {
-      $tabsWrapperEl
-      .find('.tab__content')
-      .hide();
+    $title.on("click", () => {
+      $tabsWrapperEl.find(".tab__content").hide();
 
       $tabsWrapperEl
-        .find('.tab__content')
+        .find(".tab__content")
         .eq(i)
         .show();
 
       $tabsWrapperEl
-      .find('.tab__title--active')
-      .removeClass('tab__title--active');
+        .find(".tab__title--active")
+        .removeClass("tab__title--active");
 
-      $title
-      .toggleClass('tab__title--active');
-
+      $title.toggleClass("tab__title--active");
     });
   });
+});
+
+$(document).ready(function() {
+  $(".photoshoot").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    variableWidth: true
+  });
+
+  if (window.matchMedia("(min-width: 768px)").matches) {
+  $(".portfolio-images").slick({
+  });
+  }
 });
